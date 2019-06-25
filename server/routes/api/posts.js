0de84693e19bfe775127c424a2a3402a21e9a714
@darrennicol75 <darren.nicol75@gmail.com>
2019-06-25 22:33:05 +0100

    Initial Commit of Full stack tutorial

---
+++ server/routes/api/posts.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const mongodb = require('mongodb');
+
+const router = express.Router();
+
+//Get posts
+router.get('/', async (req, res) => {
+  const posts = await lodaPostsCollection();
+  res.send(await posts.find({}).toArray());
+})
+
+//Add posts
+router.post('/', async (req, res) => {
+  const posts = await loadPostsCollection();
+  await posts.insertOne({
+    text: req.body.text,
+    createdAt: new Date ()
+  });
+  res.status(201).send();
+})
+
+//Delete posts
+
+router.delete('/:id', async (req, res) => {
+  const posts = await loadPostsCollection();
+  await posts.deleteOne({_id: new mongodb.objectID(req.params.id)});
+  res.status(200).send();
+})
+
+
+async function loadPostsCollection() {
+  const client = await mongodb.MongoClient.connect
+  ('mongodb://---------', {
+    useNewUrlParser: true
+  });
+  return.client.db('vue_express').collection('posts');
+}
+
+module.exports = router;
